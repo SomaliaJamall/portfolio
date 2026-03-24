3e4e9a15204d3becee0006a234c1bc70612f8099
Somalia Jamall <somaliajamall@gmail.com>
2026-03-24 14:30:43 -0400

    Update outDir

--- my-portfolio/vite.config.ts
+++ my-portfolio/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '/var/www/',
+    outDir: '/var/www/html',
     emptyOutDir: true, // also necessary
   },
 })
