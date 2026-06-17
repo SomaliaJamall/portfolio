61ba3a19000cc7d0d24422d98c9ce3b145dc66c4
Somalia Jamall <somaliajamall@gmail.com>
2026-06-17 13:12:09 -0400

    Componentize resume and add portfolio

---
+++ my-portfolio/src/portfolio.tsx
@@ -0,0 +1,80 @@
+
+export default function Portfolio({ }) {
+    return (
+        <>
+            <section className="portfolio-section">
+                <h2>Auto Suggest a Purchase</h2>
+                <img src="ASAP1.png" style={{ width: "100%" }} />
+                <div className="portfolioContent">
+                    <div className="subsection">
+                        <div className="overline">Roles:</div>
+                        Project Manager, Designer, Developer
+                    </div>
+                    <div className="subsection">
+                        <div className="overline">Languages:</div>
+                        React, PostgreSQL, PHP, Python
+                    </div>
+                </div>
+                <div className="subsection">
+                    <h3>The Problem: <span className="headerEmphasize">A Time Consuming Manual Process</span></h3>
+                    The pre-existing process required librarians to manually track hundreds of book
+                    purchase suggestions with emails and spreadsheets. This process was time consuming
+                    and lacked transparency as communication often faltered between patrons and
+                    librarians with extensive responsibilities.
+                </div>
+                <div className="subsection">
+                    <h3>The Solution: <span className="headerEmphasize">Use tech to automate and streamline</span> </h3>
+                    Auto Suggest a Purchase (ASAP): an application built with REACT, SQL, and PHP
+                    that automates library-patron communications and streamlines book suggestions
+                    by collecting patron suggestions for staff within a dashboard and automating
+                    communications and holds.
+                </div>
+                <div className="subsection">
+                    <h3>The Outcome: <span className="headerEmphasize">An Award Winning Application</span></h3>
+                    Auto Suggest a Purchase has been locally and nationally recognized with the
+                    program winning several awards, including a national library hackathon and an
+                    innovation award from the North East Florida Library Information Network.
+                    Additionally, I have collaborated with libraries in California, New York,
+                    and Indiana to implement Auto Suggest a Purchase for their patrons following
+                    the success demonstrated in the usage of the Jacksonville Public Library.
+                </div>
+                <div className="subsection">
+                    <h3>My Process</h3>
+                    <img src="asapProcessMap.png" className="portfolioImg" />
+                    <div className="imageCaption">
+                        <div className="subsection">
+                            <div className="overline">Mapping the existing state</div>
+                            I always start new projects by working with stakeholders to build an 
+                            understanding of where they are today
+                        </div>
+                    </div>
+                    <img src="asapIdealProcessMap.png" className="portfolioImg" />
+                    <div className="imageCaption">
+                        <div className="subsection">
+                            <div className="overline">Mapping the ideal process</div>
+                            After I develop an understanding of how things are, I work with stakeholders
+                            to develope a vision for how things should be.
+                        </div>
+                    </div>
+                    <img src="asapTechnicalDiagram.png" className="portfolioImg" />
+                    <div className="imageCaption">
+                        <div className="subsection">
+                            <div className="overline">Technical Diagram</div>
+                            Prior to writing any code, I map out how information moves between systems
+                        </div>
+                    </div>
+                    <img src="asapCostBeniMatrix.png" className="portfolioImg" />
+                    <div className="imageCaption">
+                        <div className="subsection">
+                            <div className="overline">Technical Diagram</div>
+                            To plan and prioritize feature development, I work with stakeholders to create
+                            a cost/benifit matrix. Those features that are easy to implement and high value get
+                            worked on first.
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        </>
+    )
+}
---
+++ my-portfolio/src/resume.tsx
@@ -0,0 +1,186 @@
+
+export default function Resume({ }) {
+    return (
+        <>
+            <section id="resume">
+                <div id="resume-jobs">
+                    <h2>
+                        <span className="material-symbols-outlined sectionIcon">
+                            docs
+                        </span>
+                        Experience
+                    </h2>
+                    <div className='resume-section'>
+                        <p className="overline">
+                            Full Stack Application Developer
+                        </p>
+                        <p>
+                            TxRx Data, Jacksonville FL
+                        </p>
+                        <p>
+                            October 2024 - Current
+                        </p>
+                        <p>
+                            <ul>
+                                <li>
+                                    Managed a team of developers in creating an application encompassing a frontend, REST API, and backend for a video transmission application, leading to the production of a market ready product.
+                                </li>
+                                <li>
+                                    Wrote and maintained code using React, Node.js, Git, C++, and other tools producing a well documented and scalable code repository.
+                                </li>
+                            </ul>
+                        </p>
+                    </div>
+                    <div className='resume-section'>
+                        <p className="overline">
+                            Application Developer
+                        </p>
+                        <p>
+                            Jacksonville Public Library, Jacksonville FL
+                        </p>
+                        <p>
+                            September 2024 - Current
+                        </p>
+                        <p>
+                            <ul>
+                                <li>
+                                    Spearheaded the development of tools for staff and patrons using React, PHP, Python, SQL Server, and other languages/tools depending on the use case to produce web applications supporting the Jacksonville Public Library’s over 1 million patrons and over 500 staff in a way that prioritized scalability and usability, leading to an increase in staff and patron satisfaction.
+                                </li>
+                                <li>
+                                    Managed the product lifecycle including defining requirements by performing UX research and cost/benefit analysis.
+                                </li>
+                                <li>
+                                    Managed the CI/CD pipeline for library applications using Azure to ensure all code is well tested and production ready.
+                                </li>
+                                <li>
+                                    Created and documented system processes to increase cross team transparency and collaboration.
+                                </li>
+                            </ul>
+                        </p>
+                    </div>
+                    <div className='resume-section'>
+                        <p className="overline">
+                            Senior UX Designer
+
+                        </p>
+                        <p>
+                            Arrive Logistics, Austin TX (Onsite)
+                        </p>
+                        <p>
+                            January 2021 - June 2023
+                        </p>
+                        <p>
+                            <ul>
+                                <li>
+                                    Orchestrated research initiatives to untangle intricate business challenges across diverse teams, leading to a 20% increase in commission accuracy and a 30% reduction in shipment processing time.
+                                </li>
+                                <li>
+                                    Produced UI mocks and prototypes using Figma, expediting initial idea validation and ensuring seamless developer handoff.
+                                </li>
+                                <li>
+                                    Guided and mentored junior designers in the nuances of UX research techniques, fostering their skills, resulting in improvements in their ability to conduct insightful user interviews
+                                </li>
+                            </ul>
+                        </p>
+                    </div>
+                    <div className='resume-section'>
+                        <p className="overline">
+                            UX Engineer
+                        </p>
+                        <p>
+                            Blackbaud Inc, Charleston SC (Hybrid)
+                        </p>
+                        <p>
+                            June 2019 - December 2020
+                        </p>
+                        <p>
+                            <ul>
+                                <li>
+                                    Utilized Angular to build components for an internal design system leading to faster design and development of new features.
+                                </li>
+                                <li>
+                                    Gathered feedback from  UX designers to create processes for design documentation and prototyping leading to better cohesion between projects.
+                                </li>
+                                <li>
+                                    Collaborated with product managers and developers to perform low/high fidelity prototyping, and usability testing to support rapid idea validation.
+                                </li>
+                            </ul>
+                        </p>
+                    </div>
+                    <div className='resume-section'>
+                        <p className="overline">
+                            Lead Full Stack Developer
+
+                        </p>
+                        <p>
+                            T21 Technologies, Fl
+                        </p>
+                        <p>
+                            April 2016 - December 2017
+                        </p>
+                        <p>
+                            <ul>
+                                <li>
+                                    Worked in React, Python, C++, JavaScript, HTML, and CSS to write full-stack code leading to an application that generated revenue for the company and later sold
+                                </li>
+                                <li>
+                                    Managed the code repository using GIT to support developer collaboration and code maintainability.
+                                </li>
+                                <li>
+                                    Trained and managed a team of interns and junior developers to increase the overall quality of code written by all contributors.
+                                </li>
+                                <li>
+                                    Collaborated with the CEO and international partners to prioritize and plan the development of new features insuring that high priority features were delivered rapidly
+                                </li>
+                            </ul>
+                        </p>
+                    </div>
+                    <div className='resume-section'>
+                        <p className="overline">
+                            Full Stack Developer
+                        </p>
+                        <p>
+                            T21 Technologies, Fl
+                        </p>
+                        <p>
+                            June 15 - May 16
+                        </p>
+                        <p>
+                            <ul>
+                                <li>
+                                    Collaborated with other engineers to write full-stack code for an embedded system using Python, C, React,  Javascript, HTML, and CSS to produce well documented and easy to maintain code.
+                                </li>
+                            </ul>
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <section id="education">
+                <div id="schools">
+                    <h2>
+                        <span className="material-symbols-outlined sectionIcon">
+                            school
+                        </span>
+                        Education
+                    </h2>
+                    <div className='degree'>
+                        <p className="overline">
+                            Master of Human-Computer Interaction Design
+                        </p>
+                        <p>
+                            The University of Indiana Bloomington (August 2017 - May 2019)
+                        </p>
+                    </div>
+                    <div className='degree'>
+                        <p className="overline">
+                            Bachelor of Science in Computer Science
+                        </p>
+                        <p>
+                            The University of North Florida, Jacksonville, FL(August 2012 - April 2016)
+                        </p>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
